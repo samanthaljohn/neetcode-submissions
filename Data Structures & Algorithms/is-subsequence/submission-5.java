345590358d20465f45ea242d89b213d054c5d15d
Samantha John <john.samanthalynn@gmail.com>
2026-07-30 01:05:59 -0600

    Add: is-subsequence - submission-5

---
+++ Data Structures & Algorithms/is-subsequence/submission-5.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int sPtr = 0, tPtr = 0;
+
+        while (sPtr < s.length() && tPtr < t.length()){
+            if (s.charAt(sPtr) == t.charAt(tPtr)){
+                sPtr++;
+            }
+            tPtr++;
+        }
+
+        return sPtr == s.length(); 
+    }
+}
